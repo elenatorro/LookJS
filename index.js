711b5a9a616d64e6828ae09c5ac749fac1d5bc2d
Elena Torro <elenatorro@gmail.com>
2014-11-06 12:54:32 +0100

    node modules dependences changed

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-/**
- * Dependencies
- */
-var CoffeeScript= require("coffee-script");
-
-// Register CoffeeScript if exits
-if(CoffeeScript.register) CoffeeScript.register();
-
 var board = require('./lib/Board');
 
 /**
