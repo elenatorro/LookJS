21d8f007b1ee416c45b18db19beff26f0ca571ac
Elena Torro <elenatorro@gmail.com>
2014-11-07 20:09:36 +0100

    trying to tests divs, but not working

--- test/test.js
+++ test/test.js
@@ -1,15 +1,14 @@
 // -- Dependencies ------------------------------------------------------
-
 var should = require("should");
 var Look = require("../lib/Look");
 
 // -- Tests -------------------------------------------------------------
 
-describe('Default Test: ', function(){
-  describe('Group 2', function(){
-    it('should return -1 when the value is not present', function(){
+describe('Creating DOM elements (not inserting): ', function(){
+  describe('Creating DIVS, very simple', function(){
+    it('Create a new simple div', function(){
       var look = new Look();
-      look.defaultVariable().should.eql('default');
+      look.createDiv().should.eql('<div></div>');
     })
   })
 })
