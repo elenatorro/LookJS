1b7fbe2db1f696eb4a52ab8d478bb271bdbfa585
Elena Torro <elenatorro@gmail.com>
2014-11-08 12:47:04 +0100

    testing css functions

--- test/qunit_tests.js
+++ test/qunit_tests.js
@@ -1,10 +1,50 @@
 var Look = new Look();
 
+
+/*############## TEST FUNCTIONS ##############*/
+
+
+/*###### TAG CREATION ######*/
 function testDivCreation(name, father) {
   QUnit.test(name, function(assert) {
-    var son = Look.createDiv(father.id);
+    var son = Look.createDiv(father);
     assert.equal(father.lastChild, son)
   })
 }
 
-testDivCreation("Look.createDiv, father", document.getElementById('father'));
+
+/*###### PLAYING WITH CSS ######*/
+function testAddingBackgroundColor(name, element, options) {
+  QUnit.test(name, function(assert) {
+    Look.cssOptions(element, options);
+    assert.equal(element.style.backgroundColor, 'blue');
+  })
+}
+
+function testAddingCssOptions(name, element, options) {
+  QUnit.test(name, function(assert) {
+    Look.cssOptions(element, options);
+    assert.equal(element.style.width, options.width);
+    assert.equal(element.style.height, options.height);
+    assert.equal(element.style.backgroundColor, options.backgroundColor);
+  })
+}
+
+/*############## CALLING FUNCTIONS ##############*/
+testDivCreation("Look.createDiv, father",
+                  document.getElementById('father')
+                );
+
+testAddingBackgroundColor("Look.cssOptions, element, options",
+                  document.getElementById('father'),
+                  {'backgroundColor': 'blue'}
+                );
+
+testAddingCssOptions("Look.cssOptions, element, options",
+                  document.getElementById('father'),
+                    {
+                      'backgroundColor': 'blue',
+                      'width': '100px',
+                      'height': '100px'
+                    }
+                );
