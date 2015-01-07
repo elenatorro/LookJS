66103da639c707775aa0ffa1630af6ef2ff8c266
Elena Torro <elenatorro@gmail.com>
2015-01-07 21:18:36 +0100

    added tests for translations

--- test/qunit_tests_2.js
+++ test/qunit_tests_2.js
@@ -5,7 +5,6 @@
 function testLinkCreation(href, linkElement) {
   QUnit.test(href, function(assert) {
     var link = Look.getBy.href(href);
-    console.log(link);
     assert.equal(linkElement.firstChild, link)
   })
 }
@@ -24,3 +23,44 @@ var linkOptions = {
 }
 
 testLinkCreation('http://elenatorro.com', Look.create(linkOptions));
+
+function testLinkError(src, linkElement) {
+  QUnit.test(src, function(assert) {
+    var link = Look.getBy.src(src);
+    assert.equal(false, link)
+  })
+}
+
+var linkOptions = {
+  'tag':'div',
+  'attr': {
+    'id':'src fake container'
+  },
+  'content': [{
+    'tag':'a',
+    'attr': {
+      'href':'http://elenatorro.com'
+    }
+  }]
+}
+
+testLinkError('http://elenatorro.com', Look.create(linkOptions));
+
+
+/* TRANSLATION */
+
+function testTranslation(localizer, text_element) {
+  QUnit.test(localizer, function(assert) {
+    var link = Look.changeLanguage('EN', localizer);
+    assert.equal(text_element.innerHTML, 'Welcome')
+  })
+}
+
+var localize = {
+  'test_text' : {
+    ES : 'Bienvenidos',
+    EN : 'Welcome'
+  }
+};
+
+testTranslation(localize, document.getElementById('test_text'));
