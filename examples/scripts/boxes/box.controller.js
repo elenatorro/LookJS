5e436129f06f724a4322e8f07de0f537bf32620f
Elena Torro <elenatorro@gmail.com>
2015-07-29 22:56:15 +0200

    examples improved

--- examples/scripts/boxes/box.controller.js
+++ examples/scripts/boxes/box.controller.js
@@ -1,2 +1,9 @@
-var box = Look.createFromModel('Box');
-Look.style.setStyle(box, {backgroundColor: 'green'});
+var box = Look.createFromComponent('Box');
+Look.style.setStyle(box, {borderColor: 'red', borderStyle: 'solid', borderWidth: '3px'});
+
+Look.createFromComponent('Box');
+Look.components.extendAttribute('Box', 'BigBox', {attr: {class: 'big'}});
+
+Look.createFromComponent('BigBox');
+Look.components.merge('Circle', 'Box', 'CircleBox');
+Look.createFromComponent('CircleBox');
