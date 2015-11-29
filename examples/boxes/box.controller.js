a1b19cabb88cd4379e6d7dbeebda2cbbfcac8cd3
Elena Torro <elenatorro@gmail.com>
2015-11-29 20:30:34 +0100

    updated

--- examples/boxes/box.controller.js
+++ examples/boxes/box.controller.js
@@ -1,44 +1,42 @@
-var box = Look.createFromComponent('Box');
+var box = Look.createFromComponent('Box')
 
+Look.style.setStyle(box, {borderColor: 'orange', borderStyle: 'solid', borderWidth: '3px'})
+Look.createFromComponent('Box')
 
-Look.style.setStyle(box, {borderColor: 'orange', borderStyle: 'solid', borderWidth: '3px'});
-Look.createFromComponent('Box');
+Look.components.extendAttribute('Box', 'BigBox', {attr: {class: 'big'}})
 
-Look.components.extendAttribute('Box', 'BigBox', {attr: {class: 'big'}});
+Look.createFromComponent('BigBox')
 
-Look.createFromComponent('BigBox');
+Look.components.merge('Circle', 'Box', 'CircleBox')
 
-Look.components.merge('Circle', 'Box', 'CircleBox');
+var circle = Look.createFromComponent('CircleBox')
 
-var circle = Look.createFromComponent('CircleBox');
+Look.style.setStyle(box, {width: '400px'})
 
-Look.style.setStyle(box, {width: '400px'});
-
-
-var resize = function(width) {
-	var model = this.model;
-	Look.style.setStyle(model, {width: width})
-};
-
-var scale = function(size) {
-	var model = this.model;
-	Look.style.setStyle(model, {width: size, height: size})
-};
-
-Look.controllers.extend(box,{resize: resize});
-setTimeout(function(){ box.operations.resize('800px'); }, 500);
-
-Look.controllers.create(circle, {resize: resize});
-
-function delay(callback) {
-    setTimeout(function() {callback()}, 2000);
+var resize = function (width) {
+  var model = this.model
+  Look.style.setStyle(model, {width: width})
 }
 
-var setTime = function(callback){
-    delay(function(){
-    	callback();
-    });
+var scale = function (size) {
+  var model = this.model
+  Look.style.setStyle(model, {width: size, height: size})
 }
 
-Look.controllers.extend(circle, {scale: scale});
-setTime(function(){circle.operations.scale('500px')});
+Look.controllers.extend(box, {resize: resize})
+setTimeout(function () { box.operations.resize('800px'); }, 500)
+setTimeout(function () { box.operations.addClass(box, 'dark'); }, 500)
+Look.controllers.create(circle, {resize: resize})
+
+function delay (callback) {
+  setTimeout(function () {callback()}, 2000)
+}
+
+var setTime = function (callback) {
+  delay(function () {
+    callback()
+  })
+}
+
+Look.controllers.extend(circle, {scale: scale})
+setTime(function () {circle.operations.scale('500px')})
